bcee41083380a5906fb6e0b67f8156ed17c91de4
Harshit Mishra <hmharsh123@gmail.com>
2023-05-01 16:26:33 +0530

    4 functions Calculator

--- Calc.py
+++ Calc.py
@@ -1,18 +1,22 @@
-# This function adds and subtracts two numbers 
+# This is complete calculator
 import Add
 import Sub
+import Multiplication
+import Division
 
 # This is to present a menu to the user
 print("Select operation.")
 print("a.Addition")
 print("b.Subtract")
+print("c.Multiply")
+print("d.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(a/b): ")
+    choice = input("Enter choice(a/b/c/d): ")
     
-    # check if choice is one of the two options
-    if choice in ('a', 'b'):
+    # check if choice is one of the four options
+    if choice in ('a', 'b','c','d'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -25,6 +29,13 @@ while True:
 
         elif choice == 'b':
             print(num1, "-", num2, "=", Sub.subtract(num1, num2))
+
+        elif choice == 'c':
+            print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
+
+        elif choice == 'd':
+            print(num1, "/", num2, "=", Division.divide(num1, num2))
+
 
         # check if user wants another calculation
         # break the while loop if answer is no

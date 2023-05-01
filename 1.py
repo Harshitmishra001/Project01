854ea36a3cf812f55776c2eb4435f3091db92101
Devishabhargava <132033569+Devishabhargava@users.noreply.github.com>
2023-05-01 09:48:51 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
 print ("Addition Calculator")
+num1 = float(input("enter first number here: "))
+num2 = float(input(enter second number here: "))
+                   
+print ("press 1 for addition")
+choice = int(input(enter your choice from 1-4"))
+if choice == 1:
+                   print (num1 + num2)

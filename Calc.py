97faf3949ded4789327c3bd4af3bc1fcfd22c026
Harshit Mishra <hmharsh123@gmail.com>
2023-05-01 16:20:11 +0530

    Add and subtract Calc

--- Calc.py
+++ Calc.py
@@ -1,7 +1,38 @@
+# This function adds and subtracts two numbers 
+import Add
+import Sub
+
+# This is to present a menu to the user
 print("Select operation.")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-print("5. Power")
+print("a.Addition")
+print("b.Subtract")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(a/b): ")
+    
+    # check if choice is one of the two options
+    if choice in ('a', 'b'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == 'a':
+            print(num1, "+", num2, "=", Add.add(num1, num2))
+
+        elif choice == 'b':
+            print(num1, "-", num2, "=", Sub.subtract(num1, num2))
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
+
+
 
